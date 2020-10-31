5251e9b5acfa3cb094e5ef217ce526abd607f52a
Andy Holmes <beawareherenow@gmail.com>
2020-10-31 13:54:48 -0400

    initial

---
+++ v2Add.js
@@ -0,0 +1,14 @@
+/* Given the following code, modify it so that data can be passed
+ * into the function and added to the todos array.
+ */
+
+ todos = ["Item 2", "Item 3", "Item 4", "another item"]
+
+// function add() {
+function add(todo) { 
+  // todos.push('another item');
+  todos.push(todo);
+  console.log(todos);
+}
+
+add('Watch the next video in Practical JavaScript');
